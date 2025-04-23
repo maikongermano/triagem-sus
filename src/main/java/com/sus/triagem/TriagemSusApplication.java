221425c6418a057acfef48deac48f2a1bca75d05
Maikon Germano <mk_germano@hotmail.com>
2025-04-23 15:29:57 -0300

    fix: correção na aplicação

---
+++ src/main/java/com/sus/triagem/TriagemSusApplication.java
@@ -0,0 +1,11 @@
+package com.sus.triagem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TriagemSusApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TriagemSusApplication.class, args);
+    }
+}
